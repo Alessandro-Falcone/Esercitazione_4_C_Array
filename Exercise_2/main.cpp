9139ec0e0c0a94b05575b8aa5fb44a5f676b75ae
alexf <alexf@LAPTOP-L8J25IH7>
2024-04-11 00:15:22 +0200

    main modificato

--- Exercise_2/main.cpp
+++ Exercise_2/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
+
+
 
 int main(){
 
@@ -15,56 +18,58 @@ int main(){
 
     string line;
     double S = 0.0;
-    unsigned int n = 0;
+    size_t n = 0;
     string w;
     string r;
-
 
     getline(fileInput, line, ';');
     getline(fileInput, line);
     S = stod(line);
-    cout << "S=" << S << endl;
 
     getline(fileInput, line, ';');
     getline(fileInput, line);
     n = stoi(line);
-    cout << "n=" << n << endl;
 
     double *w1 = new double[n];
-    double *v1 = new double[n];
+    double *r1 = new double[n];
     getline(fileInput, line, ';');
     w = line;
-    cout << line << endl;
     getline(fileInput, line);
     r = line;
-    cout << line << endl;
 
     for(unsigned int i = 0; i < n; i++){
         getline(fileInput, line, ';');
         w1[i] = stod(line);
         getline(fileInput, line);
-        v1[i] = stod(line);
+        r1[i] = stod(line);
     }
-
+    cout << "S = " << fixed << setprecision(2) << S << ", ";
+    cout << "n = " << n << endl;
+    cout << "w = [ ";
     for(unsigned int i = 0; i < n; i++){
         cout << w1[i] << " ";
     }
-    cout << "\n";
+    cout << "]" << endl;
+
+    cout << "v = [ ";
     for(unsigned int i = 0; i < n; i++){
-        cout << v1[i] << " ";
+        cout << r1[i] << " ";
     }
-    cout << "\n";
+    cout << "]" << endl;
+    double rateOfReturn = 0;
+    double V = S;
+    for(unsigned int i=0;i<n; i++){
+        rateOfReturn = w1[i]*r1[i] + rateOfReturn;
+        // cout << setprecision(4) << rateOfReturn  << " ";
+        V = S*w1[i]*r1[i] + V;
+        // cout << setprecision(2) << V << " ";
+    }
+
+    cout << "Rate of return of the portfolio: " << setprecision(4) << rateOfReturn << endl;
+    cout << "V: " << setprecision(2) << V << endl;
+
     delete[] w1;
-    delete[] v1;
-
-
-
-
-
-
-    while (getline(fileInput, line)) {
-        cout << line << endl;
-    }
+    delete[] r1;
 
     fileInput.close();
 
