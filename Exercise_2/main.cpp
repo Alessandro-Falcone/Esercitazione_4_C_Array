cf6f98599bc6d2d66f3f25aef4c4982a93d85993
alexf <alexf@LAPTOP-L8J25IH7>
2024-04-10 23:30:46 +0200

    main modificato

--- Exercise_2/main.cpp
+++ Exercise_2/main.cpp
@@ -4,30 +4,69 @@
 
 using namespace std;
 
-int main() {
-    ifstream file("data.csv"); // Sostituisci "nome_file.txt" con il nome effettivo del tuo file
+int main(){
 
-    if (!file.is_open()) {
-        cerr << "Impossibile aprire il file." << endl;
+    ifstream fileInput("data.csv"); // Sostituisci "nome_file.txt" con il nome effettivo del tuo file
+
+    if (fileInput.fail()){
+        cerr << "Errore nell'apertura del file" << endl;
         return 1;
     }
 
     string line;
-    unsigned int size;
-    while (getline(file, line)) {
-        string filteredLine;
-        size = line.length();
-        for (unsigned int i = 0; i < size; i++) {
-            char c = line[i];
-            if (c != ';') {
-                filteredLine += c;
-            }
-        }
-        cout << filteredLine << endl;
+    double S = 0.0;
+    unsigned int n = 0;
+    string w;
+    string r;
+
+
+    getline(fileInput, line, ';');
+    getline(fileInput, line);
+    S = stod(line);
+    cout << "S=" << S << endl;
+
+    getline(fileInput, line, ';');
+    getline(fileInput, line);
+    n = stoi(line);
+    cout << "n=" << n << endl;
+
+    double *w1 = new double[n];
+    double *v1 = new double[n];
+    getline(fileInput, line, ';');
+    w = line;
+    cout << line << endl;
+    getline(fileInput, line);
+    r = line;
+    cout << line << endl;
+
+    for(unsigned int i = 0; i < n; i++){
+        getline(fileInput, line, ';');
+        w1[i] = stod(line);
+        getline(fileInput, line);
+        v1[i] = stod(line);
     }
 
-    file.close();
+    for(unsigned int i = 0; i < n; i++){
+        cout << w1[i] << " ";
+    }
+    cout << "\n";
+    for(unsigned int i = 0; i < n; i++){
+        cout << v1[i] << " ";
+    }
+    cout << "\n";
+    delete[] w1;
+    delete[] v1;
+
+
+
+
+
+
+    while (getline(fileInput, line)) {
+        cout << line << endl;
+    }
+
+    fileInput.close();
 
     return 0;
 }
-
