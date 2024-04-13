c430256752dee30a421d9f933e461238193216d8
alexf <alexf@LAPTOP-L8J25IH7>
2024-04-13 17:18:09 +0200

    modifiche

--- Exercise_2/src/Utils.cpp
+++ Exercise_2/src/Utils.cpp
@@ -59,7 +59,7 @@ void calcoloRateOfReturn(const double& S, const size_t& n,
     rateOfReturn = 0;
     V = S;
     for(unsigned int i = 0; i < n; i++){
-        rateOfReturn = w[i]*r [i] + rateOfReturn;
+        rateOfReturn = w[i]*r[i] + rateOfReturn;
         V = S*w[i]*r[i] + V;
     }
 }
