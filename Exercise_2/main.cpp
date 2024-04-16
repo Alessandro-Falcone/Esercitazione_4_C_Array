b4005890e4a70bde0d62a4b922426eb5ad925c72
alexf <alexf@LAPTOP-L8J25IH7>
2024-04-17 00:23:58 +0200

    modifiche

--- Exercise_2/main.cpp
+++ Exercise_2/main.cpp
@@ -17,52 +17,52 @@ int main(){
     double rateOfReturn = 0;
     double V = 0;
 
-    if(letturaDatiFileInput(fileInput, S, n, w, r) != true){ // Controllo che il file di input sia stato aperto correttamente
+    if(!letturaDatiFileInput(fileInput, S, n, w, r)){ // Controllo che il file di input sia stato aperto correttamente
 
         // dato che la funzione letturaDatiFileInput l'ho definita come una funzione booleana che ritorna "true" o "false"
         // se questa funzione ritorna "false" apparirà sullo schermo un messaggio d'errore
         // in quanto ci sarà stato qualche problema nell'apertura del file di input
 
         // messaggio di errore che dice che il file di input inserito non e' stato aperto correttamente
-        cerr << "Errore nell'apertura del file di input" << endl;
+        cerr << "Errore: impossibile aprire il file di input." << endl;
 
         // inoltre l'esecuzione del programma si arresta e oltre al messaggio d'errore il programma ritorna 1
         return 1;
 
     }else{
 
+        // Calcolo del tasso di rendimento e V
         calcoloRateOfReturn(S, n, w, r, rateOfReturn, V);
-        // vado a chiamare la funzione calcoloRateOfReturn che calcola RateOfReturn e V
+        // vado a chiamare la funzione calcoloRateOfReturn che calcola il tasso di rendimento e V
 
-        cout << "Successo nella lettura dei dati del file di input" << endl;
+        cout << "Successo: dati del file di input letti correttamente." << endl;
         cout << "Contenuto che andro' a scrivere sul file di output" << endl;
     }
 
+    // Stampa dei dati
     cout << "S = " << fixed << setprecision(2) << S << ", n = " << n << endl;
-
     cout << "w = " << arrayToString(n, w) << endl;
     cout << "v = " << arrayToString(n, r) << endl;
-
     cout << "Rate of return of the portfolio: " << setprecision(4) << rateOfReturn << endl;
     cout << "V: " << setprecision(2) << V << endl;
 
     string fileOutput = "./result.txt";
 
-    if(scritturaSuFileOutput(fileOutput, S, n, w, r, rateOfReturn, V) != true){ // Controllo che il file di output sia stato aperto correttamente
+    if(!scritturaSuFileOutput(fileOutput, S, n, w, r, rateOfReturn, V)){ // Controllo che il file di output sia stato aperto correttamente
 
         // dato che la funzione scritturaSuFileOutput l'ho definita come una funzione booleana che ritorna "true" o "false"
         // se questa funzione ritorna "false" apparirà sullo schermo un messaggio d'errore
         // in quanto ci sarà stato qualche problema nell'apertura del file di output
 
         // messaggio di errore che dice che il file di output inserito non e' stato aperto correttamente
-        cerr << "Errore nell'apertura del file di output che hai inserito\n" << endl;
+        cerr << "Errore: Impossibile aprire il file di output." << endl;
 
         // inoltre l'esecuzione del programma si arresta e oltre al messaggio d'errore il programma ritorna 1
         return 1;
 
     }else{
 
-        cout << "Successo della scrittura sul file di output" << endl;
+        cout << "Successo: risultati scritti correttamente sul file di output." << endl;
     }
 
     delete[] w;
@@ -77,13 +77,6 @@ int main(){
     // Rendendo la memoria precedentemente occupata dall'array disponibile per altre allocazioni di memoria.
     // È importante utilizzare delete[] quando si dealloca la memoria di un array allocato dinamicamente
     // per garantire la corretta gestione della memoria.
-
-
-    // S = 1000.00, n = 8
-    // w = [ 0.05 0.2 0.12 0.18 0.15 0.15 0.1 0.05 ]
-    // r = [ 0.1 0.01 0.05 0.02 0.02 0.05 0.01 0.03 ]
-    // Rate of return of the portfolio: 0.0296
-    // V: 1029.60
 
     return 0;
 }
